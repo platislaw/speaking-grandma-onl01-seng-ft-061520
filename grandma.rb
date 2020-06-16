d50e50fde9a84a071ea78654fe0ddf17e7b59259
Paul Latislaw <paullatislaw@gmail.com>
2020-06-16 22:36:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
-    else 
+    else
       "NO, NOT SINCE 1938!"
   end
 end
